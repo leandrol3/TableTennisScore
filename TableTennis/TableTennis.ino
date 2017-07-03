fd56b4d8b00818d09d223edf18db15421673296e
Leandro Lopes <leandro.lopes@bmind.ai>
2017-07-03 09:24:37 -0300

    feat: Contador de Set
    Adicionado contador de Set
    Piscar ao chega em 11
    Botao Eq apaga ao apertar duas vezes

--- TableTennis/TableTennis.ino
+++ TableTennis/TableTennis.ino
@@ -10,8 +10,7 @@ best of 5 sets until 11 each one. Deuce
 #include <SerialDisplay.h>
 
 
-//RoboCore Remote Control Map
-
+//RoboCore Remote Control Map-----------------
 const long N0 = 0xFF6897;
 const long N1 = 0xFF30CF;
 /*const N2 FF18E7;
@@ -42,12 +41,35 @@ const long BEQU = 0xFF906F;  //Botao Zerar 0
 
 const int BPRV = 1;
 
+//Mapa do Seven Digits Display------------
+
+byte seven_seg_digits[17][7] = { { 1,1,1,1,1,1,0 },  // = Digito 0
+                                 { 0,1,1,0,0,0,0 },  // = Digito 1
+                                 { 1,1,0,1,1,0,1 },  // = Digito 2
+                                 { 1,1,1,1,0,0,1 },  // = Digito 3
+                                 { 0,1,1,0,0,1,1 },  // = Digito 4
+                                 { 1,0,1,1,0,1,1 },  // = Digito 5
+                                 { 1,0,1,1,1,1,1 },  // = Digito 6
+                                 { 1,1,1,0,0,0,0 },  // = Digito 7
+                                 { 1,1,1,1,1,1,1 },  // = Digito 8
+                                 { 1,1,1,0,0,1,1 },  // = Digito 9
+                                 { 1,1,1,0,1,1,1 },  // = Digito A
+                                 { 0,0,1,1,1,1,1 },  // = Digito B
+                                 { 1,0,0,1,1,1,0 },  // = Digito C
+                                 { 0,1,1,1,1,0,1 },  // = Digito D
+                                 { 1,0,0,1,1,1,1 },  // = Digito E
+                                 { 1,0,0,0,1,1,1 },   // = Digito F
+                                 { 0,0,0,0,0,0,0 },   // = APAGADO
+                                 };
+
+
+
 int RECV_PIN = 11;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 //Inicializa os Display 7 segmentos
-SerialDisplay placarJogador1(4,5,2); // (data, clock, number of modules)
+SerialDisplay placarJogador1(12,13,2); // (data, clock, number of modules)
 //SerialDisplay placarJogador2(X,X,2); // (data, clock, number of modules)
 
 //Pontuacao dos jogadores
@@ -58,13 +80,18 @@ byte P1Unidade = 0;
 byte P1Set = 0;
 
 
+//Jogador 2
 byte PontosJogador2 = 0;
+byte P2Dezena = 0;
+byte P2Unidade = 0;
+byte P2Set = 0;
 
 void setup()
 {
+  
   Serial.begin(9600);
-  // In case the interrupt driver crashes on setup, give a clue
-  // to the user what's going on.
+  
+  //inicializa Controle Remoto
   Serial.println("Enabling IRin");
   irrecv.enableIRIn(); // Start the receiver
   Serial.println("Enabled IRin");
@@ -72,15 +99,58 @@ void setup()
 //Diplays Off
   placarJogador1.Off(0);
   placarJogador1.Info(&Serial);
+/*
+  placarJogador2.Off(0);
+  placarJogador2.Info(&Serial);
+ 
+*/
+  //Set das portas do miniDisplay1
+  pinMode(2, OUTPUT); //Pino 2 do Arduino ligado ao segmento A  
+  pinMode(3, OUTPUT); //Pino 3 do Arduino ligado ao segmento B
+  pinMode(4, OUTPUT); //Pino 4 do Arduino ligado ao segmento C
+  pinMode(5, OUTPUT); //Pino 5 do Arduino ligado ao segmento D
+  pinMode(6, OUTPUT); //Pino 6 do Arduino ligado ao segmento E
+  pinMode(7, OUTPUT); //Pino 7 do Arduino ligado ao segmento F
+  pinMode(8, OUTPUT); //Pino 8 do Arduino ligado ao segmento G
+  pinMode(9, OUTPUT); //Pino 9 do Arduino ligado ao segmento PONTO
+
+  //Inicializa PLACAR com ZERO
+  placarJogador1.Print(P1Unidade, 2);
+  placarJogador1.Print(P1Dezena, 1);
+  sevenSegWrite(P1Set);
+  writePonto(0);
   
+
 }
+
+void writePonto(byte dot)   //Funcao que aciona o ponto no display
+{  
+  digitalWrite(9, dot);
+}
+
+void sevenSegWrite(byte digit)  //Funcao que aciona o display
+{
+  byte pin = 2;
+
+  //Percorre o array ligando os segmentos correspondentes ao digito
+  for (byte segCount = 0; segCount < 7; ++segCount)  
+  { 
+    digitalWrite(pin, seven_seg_digits[digit][segCount]);
+    ++pin;
+    //delay(500);
+  }
+    //writePonto(1);  //Liga o ponto
+   //delay(100);   //Aguarda 100 milisegundos
+    //writePonto(0);  //Desliga o ponto
+}
+
 
 void loop() {
   
   //Verifica se recebeu um comando de controle remoto
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
-
+    delay(50);
     //Enviar comando para cada botao apertado
     switch (results.value){ 
       
@@ -138,6 +208,7 @@ void loop() {
    case BEQU: //Botao Zerar
       Serial.println("Botao Eq apertado");
       alterarPlacar1(3);
+      
       break;
    
    case 0xFFFFFFFF: // Repetition
@@ -193,15 +264,93 @@ void alterarPlacar1(int comando) {
       break;
     
     case 3: //Zerar Placar 1
-      PontosJogador1 = 0;
-      P1Dezena = 0;
-      P1Unidade = 0;
-      break;
+      delay(50);
+      Serial.println(irrecv.decode(&results));
+      Serial.println(results.value, HEX);
+      Serial.println("resume");
+      irrecv.resume();  //aguarda a proxima tecla
+      Serial.println(irrecv.decode(&results));
+      Serial.println(results.value, HEX);
+      
+   
+        Serial.println("Inicio do While");
+        while (true)
+        {
+          Serial.println("While");
+          Serial.println(irrecv.decode(&results));
+          if (irrecv.decode(&results)) 
+          {
+            Serial.println(results.value, HEX);
+            if (results.value == BEQU) 
+            {
+              PontosJogador1 = 0;
+              P1Dezena = 0;
+              P1Unidade = 0;
+              P1Set = 0;
+              Serial.println("Break 1");
 
+              break;    
+            }
+            else 
+           {
+              Serial.println("Else 2");
+              Serial.println(results.value, HEX);
+              if (results.value != 0xFFFFFFFF) //Para evitar a repeticao do botao apertado por muito tempo
+              {
+                  Serial.println("Break 2");
+                  break;
+              }
+              irrecv.resume();  //aguarda a proxima tecla
+
+           }
+        }
+        
+        placarJogador1.Off(0);
+        sevenSegWrite(16);
+        
+        
+        delay(500);
+        placarJogador1.Print(P1Unidade, 2);
+        placarJogador1.Print(P1Dezena, 1);
+        sevenSegWrite(P1Set);
+
+        delay(500);
+    }
   }
   placarJogador1.Print(P1Unidade, 2);
   placarJogador1.Print(P1Dezena, 1);
-  
+  sevenSegWrite(P1Set);
+
+  if (PontosJogador1 == 11) {
+    P1Set++;
+
+    delay(500);
+    placarJogador1.Off(0);
+    delay(500);
+    placarJogador1.Print(P1Unidade, 2);
+    placarJogador1.Print(P1Dezena, 1);
+    delay(500);
+    placarJogador1.Off(0);
+    delay(500);
+    placarJogador1.Print(P1Unidade, 2);
+    placarJogador1.Print(P1Dezena, 1);
+    delay(500);
+    placarJogador1.Off(0);
+    delay(500);
+    placarJogador1.Print(P1Unidade, 2);
+    placarJogador1.Print(P1Dezena, 1);
+
+    delay(1000);
+    sevenSegWrite(P1Set);
+
+    PontosJogador1 =0;
+    P1Dezena = 0;
+    P1Unidade = 0;
+    placarJogador1.Print(P1Unidade, 2);
+    placarJogador1.Print(P1Dezena, 1);
+    //placarJogador1.Dot(2);
+
+   }
 
 }
 
